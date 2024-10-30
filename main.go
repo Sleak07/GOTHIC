c4e0a0a4b0691a6e44c1f93f9638c1cf5c5ef8fd
Bimal Mallick <ghostridergothan8460@gmail.com>
2024-10-30 23:09:27 +0530

    user input and conditions done

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import "fmt"
 
-// conversion of farehnit to celcius
+// conversion of Kelvin to Celsius
 func kelvinToCelsius(k float64) float64 {
-	k = k - 273.15
-	return k
+	return k - 273.15
 }
 
-// first go function
-
+// main function
 func main() {
 	fmt.Println("Hello after 6 years in GO")
 
@@ -22,9 +20,23 @@ func main() {
 	var myAge uint8 = 6
 	fmt.Println(myAge)
 
-	// functions in go
-
+	// converting Kelvin to Celsius
 	kelvin := 294.0
 	celsius := kelvinToCelsius(kelvin)
-	fmt.Print(kelvin, "° K is ", celsius, "° C")
+	fmt.Printf("%.2f° K is %.2f° C\n", kelvin, celsius)
+
+	// conditional statements
+	command := "go east"
+	if command == "go east" {
+		fmt.Println("You head further up the mountain.")
+	} else if command == "go inside" {
+		fmt.Println("You enter the cave where you live out the rest of your life.")
+	} else {
+		fmt.Println("Didn't quite get that.")
+	}
+	// to get the user input
+	fmt.Print("Enter a number: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+	fmt.Printf("You entered: %f\n", input)
 }
