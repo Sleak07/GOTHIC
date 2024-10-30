0f0e994db881a1ce3851c22d4c599f0fe2595a58
Bimal Mallick <ghostridergothan8460@gmail.com>
2024-10-30 21:58:16 +0530

    functions in go

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import "fmt"
 
+// conversion of farehnit to celcius
+func kelvinToCelsius(k float64) float64 {
+	k = k - 273.15
+	return k
+}
+
 // first go function
 
 func main() {
 	fmt.Println("Hello after 6 years in GO")
+
+	var myName string = "Arun"
+	fmt.Println("My name is", myName)
+
+	var number int = 10
+	fmt.Println(number)
+
+	var myAge uint8 = 6
+	fmt.Println(myAge)
+
+	// functions in go
+
+	kelvin := 294.0
+	celsius := kelvinToCelsius(kelvin)
+	fmt.Print(kelvin, "° K is ", celsius, "° C")
 }
